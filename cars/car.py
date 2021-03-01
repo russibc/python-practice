f8fa99815cbede12a2b59693691fdc75cfdcb079
Bru <bcrussi@gmail.com>
2021-03-01 01:23:02 -0300

    Dummy OOP

---
+++ cars/car.py
@@ -0,0 +1,13 @@
+class Car:
+
+    def setModel(self, model):
+        self.model = model
+
+    def setCompany(self, company):
+        self.company = company
+
+    def setPrice(self, value):
+        self.value = value
+
+    def setYearOfFabrication(self, year):
+        self.year = year
---
+++ cars/main.py
@@ -0,0 +1,19 @@
+from car import Car
+
+car1 = Car()
+car1.setCompany('Audi')
+car1.setModel('A3')
+car1.setPrice(86800)
+car1.setYearOfFabrication(2019)
+
+car2 = Car()
+car2.setCompany('Fiat')
+car2.setModel('500')
+car2.setPrice(9519)
+car2.setYearOfFabrication(2018)
+
+car3 = Car()
+car3.setCompany('Kia')
+car3.setModel('Optima')
+car3.setPrice(13000)
+car3.setYearOfFabrication(2012)
